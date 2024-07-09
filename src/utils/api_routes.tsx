7eab84893877b9156ceade44a70fbdb898b75219
Pratik <pratiksolanki2026@gmail.com>
2024-07-10 04:16:59 +0530

    added env in frontend

--- src/utils/api_routes.tsx
+++ src/utils/api_routes.tsx
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = import.meta.env.VITE_SERVER_URL;
 
 export const referralRoute = `${host}/api/v1/referral`;
 export const getCoursesRoute = `${host}/api/v1/courses`;
